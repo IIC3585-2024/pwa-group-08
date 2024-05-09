846ea09650a9b3e09fb30977a2f3058436f2b9e3
VAKELA <vakel@uc.cl>
2024-05-09 15:52:11 -0400

    lebron hames

---
+++ app.js
@@ -0,0 +1,21 @@
+async function fetchTrending() {
+    const res = await fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=25`);
+    const json = await res.json();
+
+    main.innerHTML = json.data.map(createMeme).join('\n');
+}
+
+window.addEventListener('load', async e => {
+    await fetchTrending();
+
+    if ('serviceWorker' in navigator) {
+        try {
+            navigator.serviceWorker.register('serviceWorker.js');
+            console.log('SW registered');
+
+        } catch (error) {
+            console.log('SW failed');
+
+        }
+    }
+});
