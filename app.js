3fcabf8e6d16011c7687746a8ca75c36acbe77ad
VAKELA <vakel@uc.cl>
2024-05-09 16:13:39 -0400

    service workers work

--- app.js
+++ app.js
@@ -1,21 +1,10 @@
-async function fetchTrending() {
-    const res = await fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=25`);
-    const json = await res.json();
-
-    main.innerHTML = json.data.map(createMeme).join('\n');
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/service-worker.js')
+            .then(function(registration) {
+                console.log('Service Worker registered with scope:', registration.scope);
+            }, function(err) {
+                console.log('Service Worker registration failed:', err);
+            });
+    });
 }
-
-window.addEventListener('load', async e => {
-    await fetchTrending();
-
-    if ('serviceWorker' in navigator) {
-        try {
-            navigator.serviceWorker.register('serviceWorker.js');
-            console.log('SW registered');
-
-        } catch (error) {
-            console.log('SW failed');
-
-        }
-    }
-});
---
+++ service-worker.js
@@ -0,0 +1,61 @@
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open('oppo-pwa-cache-v1')
+            .then(function(cache) {
+                return cache.addAll([
+                    '/',
+                    '/index.html',
+                    '/images/icons/icon-72x72.png',
+                    '/images/icons/icon-96x96.png',
+                    '/images/icons/icon-128x128.png',
+                    '/images/icons/icon-144x144.png',
+                    '/images/icons/icon-152x152.png',
+                    '/images/icons/icon-192x192.png',
+                    '/images/icons/icon-384x384.png',
+                    '/images/icons/icon-512x512.png',
+                    '/images/god.jpg',
+                ]);
+            })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                return response || fetch(event.request);
+            })
+    );
+});
+
+
+self.addEventListener('fetch', event => {
+    const {request} = event;
+    const url = new URL(request.url);
+    if(url.origin === location.origin) {
+        event.respondWith(cacheData(request));
+    } else {
+        event.respondWith(networkFirst(request));
+    }
+
+});
+
+async function cacheData(request) {
+    const cachedResponse = await caches.match(request);
+    return cachedResponse || fetch(request);
+}
+
+async function networkFirst(request) {
+    const cache = await caches.open('dynamic-meme');
+
+    try {
+        const response = await fetch(request);
+        cache.put(request, response.clone());
+        return response;
+    } catch (error){
+        return await cache.match(request);
+
+    }
+
+}
