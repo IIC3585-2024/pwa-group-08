b762a1bf6d54cdafb9e16e7591930dbb0ee9f373
VAKELA <vakel@uc.cl>
2024-05-10 14:33:37 -0400

    feat: change app layout and add db

--- app.js
+++ app.js
@@ -27,3 +27,24 @@ function updateCounterDisplay() {
     const counterElement = document.getElementById('counter');
     counterElement.textContent = counter;
 }
+
+function openNav() {
+    document.getElementById("sideMenu")
+        .style.width = "300px";
+    document.getElementById("contentArea")
+        .style.marginLeft = "300px";
+}
+ 
+function closeNav() {
+    document.getElementById("sideMenu")
+        .style.width = "0";
+    document.getElementById("contentArea")
+        .style.marginLeft = "0";
+}
+ 
+function showContent(content) {
+    document.getElementById("contentTitle")
+        .textContent = content + " page";
+         
+    closeNav();
+}
