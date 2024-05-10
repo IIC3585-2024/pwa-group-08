9e4dc79d716a84090d0b2b8a3bb8d68460ec2e49
VAKELA <vakel@uc.cl>
2024-05-10 18:59:44 -0400

    separar navbar

--- index.js
+++ index.js
@@ -1,24 +1,3 @@
 
-function openNav() {
-    document.getElementById("sideMenu")
-        .style.width = "300px";
-    document.getElementById("contentArea")
-        .style.marginLeft = "300px";
-}
- 
-function closeNav() {
-    document.getElementById("sideMenu")
-        .style.width = "0";
-    document.getElementById("contentArea")
-        .style.marginLeft = "0";
-}
- 
-function showContent(content) {
-    document.getElementById("contentTitle")
-        .textContent = content + " page";
-         
-    closeNav();
-}
-
 // Call addDummyData when the button is clicked
 document.getElementById('init-dummy-data').addEventListener('click', addDummyData);
