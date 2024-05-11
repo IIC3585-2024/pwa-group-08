e01b32aa73f684c19337d2b1e5bc14d0a7f20671
Vicente Cruz <vcrb@uc.cl>
2024-05-11 18:42:32 -0400

    style - fixed style for landing page & sidebar

--- js/navBar.js
+++ js/navBar.js
@@ -1,4 +1,3 @@
-
 // if(window.Notification) {
 //     if(Notification.permission === 'granted') {
 //       send_notification();
@@ -17,38 +16,27 @@
 //       icon: '<>'
 //     }
 //     let notif = new Notification('My New Notification', notificationOptions);
-  
+
 //     notif.onclick = () => {
 //       console.log('Notification clicked');
 //     }
 //   }
 
-  function requestPermission() {
-    console.log('Requesting permission...');
-    Notification.requestPermission().then((permission) => {
-      if (permission === 'granted') {
-        console.log('Notification permission granted.');
-      }
-    });
+function requestPermission() {
+  console.log("Requesting permission...");
+  Notification.requestPermission().then((permission) => {
+    if (permission === "granted") {
+      console.log("Notification permission granted.");
+    }
+  });
 }
 
 function openNav() {
-    document.getElementById("sideMenu")
-        .style.width = "300px";
-    document.getElementById("contentArea")
-        .style.marginLeft = "300px";
+  document.getElementById("sideMenu").style.width = "300px";
+  document.getElementById("contentArea").style.marginLeft = "300px";
 }
- 
+
 function closeNav() {
-    document.getElementById("sideMenu")
-        .style.width = "0";
-    document.getElementById("contentArea")
-        .style.marginLeft = "0";
-}
- 
-function showContent(content) {
-    document.getElementById("contentTitle")
-        .textContent = content + " page";
-         
-    closeNav();
+  document.getElementById("sideMenu").style.width = "0";
+  document.getElementById("contentArea").style.marginLeft = "0";
 }
