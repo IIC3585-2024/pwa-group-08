5a68e0dac81d04bd6807afdada81780d29253989
Vicente Cruz <vcrb@uc.cl>
2024-05-11 22:42:40 -0400

    Feat - All views implemented

--- js/index.js
+++ js/index.js
@@ -1,20 +1,22 @@
-
-window.addEventListener('databaseOpened', function() {
-    // Custom behavior for when the database is successfully opened
-    getEvents(renderEvents);
-  });
-
+window.addEventListener("databaseOpened", function () {
+  // Custom behavior for when the database is successfully opened
+  getEvents(renderEvents);
+});
 
 function renderEvents(events) {
-    const eventList = document.getElementById('event-list');
-    eventList.innerHTML = '';
+  const eventList = document.getElementById("eventsList");
+  eventList.innerHTML = ""; // Limpia el contenido existente
 
-    events.forEach((event) => {
-        const eventItem = document.createElement('div');
-        eventItem.textContent = event.name;
-        eventList.appendChild(eventItem);
-    });
+  events.forEach((event) => {
+    console.log(`event.name: ${event.name}`); // Log para depuración
+    const eventItem = document.createElement("div");
+    eventItem.classList.add("event-item"); // Añade la clase para estilos
+    eventItem.textContent = event.name; // Establece el nombre del evento
+    eventItem.onclick = function () {
+      // Agrega un manejador de clic
+      // Redirige al usuario a la página del evento con el nombre del evento como parámetro
+      location.href = `eventView.html?id=${encodeURIComponent(event.id)}`;
+    };
+    eventList.appendChild(eventItem); // Añade el evento al contenedor
+  });
 }
-
-// Call addDummyData when the button is clicked
-document.getElementById('init-dummy-data').addEventListener('click', addDummyData);
